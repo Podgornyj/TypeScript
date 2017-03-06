786a99ec481cad8a2bcb732b24253ab24ae0bdde
Anton Podgornyj <podgornyjanton@gmail.com>
2017-03-06 14:04:18 +0200

    HW

---
+++ HW/main.js
@@ -0,0 +1,30 @@
+var menuList = [
+    { title: 'JavaScript', items: ['React', 'Angular2', 'Cycle.js'] },
+    { title: 'Dart', items: ['Flutter', 'Angular2', 'Polymer'] },
+];
+function generateMenu(list) {
+    var content = "<ul>";
+    for (var _i = 0; _i < list.length; _i++) {
+        var a = list[_i];
+        content += "<li><a class='title'>" + a.title + "</a><ul>";
+        for (var _a = 0, _b = a.items; _a < _b.length; _a++) {
+            var item = _b[_a];
+            content += "<li><a>" + item + "</a></li>";
+        }
+        content += "</li></ul>";
+    }
+    content += "</ul>";
+    return content;
+}
+var navMenuList = document.querySelector('.menu');
+navMenuList.innerHTML = generateMenu(menuList);
+navMenuList.onclick = function (ev) {
+    var el = ev.target;
+    var classList = el.classList;
+    if (!classList.contains('title')) {
+        return;
+    }
+    var parentLi = el.parentNode;
+    parentLi.classList.toggle('menu-open');
+};
+//# sourceMappingURL=main.js.map
